986fe570f0c23b4ff5d882bf1bb16ecb7f3e9e00
deverober <robthedeveloper@icloud.com>
2023-03-18 13:42:51 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import '../scss/main.scss'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='sidebar'>
+        Sidebar
+      </div>
+
+      <header className='header'>
+        Header
+      </header>
+
+      <div className="realtors">
+        Top 3 Realtors
+      </div>
+
+      <section className="features">
+        Features
+      </section>
+
+      <div className="story__pictures">
+        Story pictures
+      </div>
+
+      <div className="story__content">
+        Story content
+      </div>
+
+      <section className="homes">
+        Homes
+      </section>
+
+      <section className="gallery">
+        Gallery
+      </section>
+
+      <footer className="footer">
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
